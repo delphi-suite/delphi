8f06bf9a2245afda2f14a0d0039818825cb7feaf
SrGonao <g-spaulo@hotmail.com>
2024-02-04 20:10:16 +0100

    Fixed bug, separated function

--- src/delphi/train/permute.py
+++ src/delphi/train/permute.py
@@ -1,7 +1,7 @@
 # Number number generation utils without using any library
 
 
-class Permutation:
+class Random:
     """
     Permutation class
     Atributes:
@@ -12,25 +12,28 @@ class Permutation:
     def __init__(self, seed):
         self.state = seed
 
-    def set_seed(self, seed):
-        """
-        Set the seed
-        """
-        self.state = seed
-
-    def permute(self, list):
-        """
-        Permute a list in place using Fisher-Yates shuffle
-        """
-        for i in range(len(list)):
-            j = int(self.random_number_generator() * (i + 1))
-            # self.state = self.state + 1
-            list[i], list[j] = list[j], list[i]
-        return list
-
-    def random_number_generator(self):
+    def random_uniform(self) -> float:
         """
         Generate a random number between 0 and 1 using minstd_rand from C++11
         """
         self.state = (self.state * 48271) % 4294967296
         return self.state / 4294967296
+
+    def random_number(self, max: int) -> int:
+        """
+        Generate a random number between 0 and max
+        """
+        return int(self.random_uniform() * max)
+
+
+def permute(seed: int, list: list, epoch: int = 0) -> list:
+    """
+    Permute a list in place using Fisher-Yates shuffle
+    """
+
+    random = Random(seed)
+    for i in range(len(list)):
+        max_index = len(list) - i
+        j = random.random_number(max_index) + i
+        list[i], list[j] = list[j], list[i]
+    return list
