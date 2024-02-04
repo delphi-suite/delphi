f9dbff24aa0cfef789a864b18cd6cd40763cda7d
Jannik Brinkmann <jannik.brinkmann698@gmail.com>
2024-02-04 23:19:40 +0100

    add llama2.c wrapper

--- src/delphi/train/llama2.py
+++ src/delphi/train/llama2.py
@@ -1,1 +1,14 @@
-import llama2.c
+from dataclasses import dataclass
+
+from llama2.model import ModelArgs, Transformer
+
+
+@dataclass
+class LLaMA2Args(ModelArgs):
+    pass
+
+
+class LLaMA2(Transformer):
+    
+    def __init__(self, params) -> None:
+        super().__init__(self, params)
