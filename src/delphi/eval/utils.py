eb038eee6cbfac068e5bcd7c6c83206ee85dd35b
Jett <jettjaniak@gmail.com>
2024-02-07 21:47:29 -0800

    eval.utils.load_validation_dataset

--- src/delphi/eval/utils.py
+++ src/delphi/eval/utils.py
@@ -1,6 +1,8 @@
 from collections.abc import Callable
+from typing import cast
 
 import torch
+from datasets import Dataset, load_dataset
 from jaxtyping import Float, Int
 
 
@@ -25,3 +27,18 @@ def get_next_logprobs(
     logprobs = get_all_logprobs(model, input_ids[:, :-1])
     next_tokens = input_ids[:, 1:]
     return gather_logprobs(logprobs, next_tokens)
+
+
+def load_validation_dataset(dataset_name: str) -> Dataset:
+    if "/" not in dataset_name:
+        dataset_name = f"delphi-suite/{dataset_name}"
+    data_str = f"data/validation-*.parquet"
+    dataset = load_dataset(
+        dataset_name,
+        data_files=data_str,
+        verification_mode="no_checks",
+        # this seems to be the only split when using data_files
+        # regardless of the files we're actually loading
+        split="train",
+    )
+    return cast(Dataset, dataset)
