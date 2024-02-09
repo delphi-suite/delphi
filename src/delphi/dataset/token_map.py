ebd0314ec01e3534ed9ee69c87ddc68175979f34
menamerai <rphananh@gmail.com>
2024-02-09 12:39:27 -0500

    moved token_map function to library

--- src/delphi/dataset/token_map.py
+++ src/delphi/dataset/token_map.py
@@ -6,6 +6,15 @@ from datasets import Dataset
 def token_map(
     tokenized_dataset: Dataset,
 ) -> dict[int, list[tuple[int, int]]]:
+    """Return a mapping of tokens to their (prompt_idx, token_idx) locations in the tokenized_dataset.
+
+    Args:
+        tokenized_dataset (Dataset): A tokenized dataset.
+
+    Returns:
+        dict[int, list[tuple[int, int]]]: A mapping of tokens to their (prompt_idx, token_idx)
+            locations in the tokenized_dataset.
+    """
     mapping = {}
     tokenized_dataset = cast(Dataset, tokenized_dataset)
     for prompt_idx, prompt in enumerate(tokenized_dataset):
