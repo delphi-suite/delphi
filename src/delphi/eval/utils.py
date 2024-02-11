90a2a16c9acd98e1b60d1dfdcb0a628cc8e9e65a
Jai <jai@jai.one>
2024-02-10 21:29:31 -0800

    Refactor eval utils.py re: CR

--- src/delphi/eval/utils.py
+++ src/delphi/eval/utils.py
@@ -1,14 +1,10 @@
 from collections.abc import Callable
-from typing import List, cast
+from typing import cast
 
 import torch
 from datasets import Dataset, load_dataset
 from jaxtyping import Float, Int
 from transformers import PreTrainedModel, PreTrainedTokenizerBase
-
-ALLOWED_CHARS = set(
-    " \n\"'(),.:?!0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-)
 
 
 def get_all_logprobs(
@@ -33,12 +29,30 @@ def gather_logprobs(
     return torch.gather(logprobs, -1, tokens.unsqueeze(-1)).squeeze(-1)
 
 
-def get_next_logprobs(
+def get_all_and_next_logprobs(
     model: Callable, input_ids: Int[torch.Tensor, "batch seq"]
-) -> Float[torch.Tensor, "batch shorter_seq"]:
+) -> tuple[
+    Float[torch.Tensor, "batch shorter_seq vocab"],
+    Float[torch.Tensor, "batch shorter_seq"],
+]:
     logprobs = get_all_logprobs(model, input_ids[:, :-1])
     next_tokens = input_ids[:, 1:]
-    return gather_logprobs(logprobs, next_tokens)
+    return logprobs, gather_logprobs(logprobs, next_tokens)
+
+
+def get_next_and_top_k_probs(
+    model: PreTrainedModel, input_ids: Int[torch.Tensor, "seq"], k: int = 3
+) -> tuple[
+    Float[torch.Tensor, "shorter_seq"],
+    torch.return_types.topk,
+]:
+    all_logprobs, next_logprobs = get_all_and_next_logprobs(
+        model, input_ids.unsqueeze(0)
+    )
+    all_probs = torch.exp(all_logprobs[0])
+    next_probs = torch.exp(next_logprobs[0])
+    top_k = torch.topk(all_probs, k, dim=-1)
+    return next_probs, top_k
 
 
 def load_validation_dataset(dataset_name: str) -> Dataset:
@@ -56,16 +70,6 @@ def load_validation_dataset(dataset_name: str) -> Dataset:
     return cast(Dataset, dataset)
 
 
-def load_text_from_dataset(dataset: Dataset) -> list[str]:
-    text = []
-    for sample_txt in dataset["story"]:
-        # encoding issues and rare weird prompts
-        if not set(sample_txt).issubset(ALLOWED_CHARS):
-            continue
-        text.append(sample_txt)
-    return text
-
-
 def tokenize(
     tokenizer: PreTrainedTokenizerBase, sample_txt: str
 ) -> Int[torch.Tensor, "seq"]:
@@ -74,22 +78,3 @@ def tokenize(
         Int[torch.Tensor, "seq"],
         tokenizer.encode(tokenizer.bos_token + sample_txt, return_tensors="pt")[0],
     )
-
-
-def get_correct_and_all_probs(
-    model: PreTrainedModel, sample_tok: Int[torch.Tensor, "seq"]
-) -> tuple[Float[torch.Tensor, "next_seq"], Float[torch.Tensor, "next_seq vocab"]]:
-    """Get probabilities for the actual next token and for all predictions"""
-    # remove the first token (the bos token)
-    probs = get_single_logprobs(model, sample_tok)[1:]
-    correct_probs = probs[range(len(probs)), sample_tok[1:]]
-    return correct_probs, probs
-
-
-def get_correct_and_top_probs(
-    model: PreTrainedModel, sample_tok: Int[torch.Tensor, "seq"], top_k: int = 3
-) -> tuple[Float[torch.Tensor, "next_seq"], torch.return_types.topk]:
-    """Get probabilities for the actual next token and for top k predictions"""
-    correct_probs, probs = get_correct_and_all_probs(model, sample_tok)
-    top_k_probs = torch.topk(probs, top_k, dim=-1)
-    return correct_probs, top_k_probs
