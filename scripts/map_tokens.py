40f83d2e2f661a9076c02c248287bd3b5839bad2
menamerai <rphananh@gmail.com>
2024-02-14 14:12:43 -0500

    added token mapping script

---
+++ scripts/map_tokens.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import argparse
+import pickle
+
+from delphi.eval.token_map import token_map
+from delphi.eval.utils import load_validation_dataset
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument(
+        "dataset_name", help="Dataset from huggingface to run token_map on"
+    )
+    parser.add_argument("--output", help="Output file name", default="token_map.pkl")
+    args = parser.parse_args()
+
+    dataset = load_validation_dataset(args.dataset_name)
+
+    mapping = token_map(dataset)
+
+    with open(f"data/{args.output}", "wb") as f:
+        pickle.dump(mapping, file=f)
