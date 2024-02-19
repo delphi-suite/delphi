11411e28f915d946ed0f7b36a8e229652347f315
Siwei Li <siwli@ea.com>
2024-02-18 20:54:11 -0800

    Update plotting by using Plotly

--- src/delphi/eval/vis_per_token_model.py
+++ src/delphi/eval/vis_per_token_model.py
@@ -1,37 +1,50 @@
-import ipywidgets as widgets
-import matplotlib.pyplot as plt
+import ipywidgets
 import numpy as np
-from ipywidgets import interact
+import plotly.graph_objects as go
+from beartype.typing import Dict
 
 
-def visualize_per_token_category(input):
+def visualize_per_token_category(input: Dict[str, Dict[str, tuple]]) -> ipywidgets.VBox:
     model_names = list(input.keys())
-    categories = list(list(input.values())[0].keys())
+    categories = list(input[model_names[0]].keys())
+    category = categories[0]
 
-    def _f(category):
+    def get_plot_values(category: str) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         x = np.array([input[name][category] for name in model_names]).T
-        means = np.mean(x, axis=0)
-        median = np.median(x, axis=0)
-        q1 = np.quantile(x, 0.25, axis=0)
-        q3 = np.quantile(x, 0.75, axis=0)
+        means, err_lo, err_hi = x[0], x[1], x[2]
+        return means, err_lo, err_hi
 
-        ax = plt.gca()
-        ax.set_ylim([-5, 5])  # TODO
-
-        plt.plot(model_names, means)
-        plt.errorbar(model_names, median, yerr=[median - q1, q3 - median], fmt="o")
-
-    interact(
-        _f,
-        category=widgets.Dropdown(
-            options=categories,
-            placeholder="",
-            description="Token Category:",
-            disabled=False,
+    means, err_low, err_hi = get_plot_values(category)
+    g = go.FigureWidget(
+        data=go.Scatter(
+            x=model_names,
+            y=means,
+            error_y=dict(
+                type="data",
+                symmetric=False,
+                array=err_hi,
+                arrayminus=err_low,
+                color="purple",
+            ),
         ),
+        layout=go.Layout(yaxis=dict(title="Loss")),
     )
 
+    selected_category = ipywidgets.Dropdown(
+        options=categories,
+        placeholder="",
+        description="Token Category:",
+        disabled=False,
+    )
 
-# Usage:
-# from dataset.mock_per_token_performance import performance_datas
-# visualize_per_token_category(performance_data)
+    def response(change):
+        if selected_category.value:
+            means, err_lo, err_hi = get_plot_values(selected_category.value)
+            with g.batch_update():
+                g.data[0].y = means
+                g.data[0].error_y["array"] = err_hi
+                g.data[0].error_y["arrayminus"] = err_lo
+
+    selected_category.observe(response, names="value")
+
+    return ipywidgets.VBox([selected_category, g])
