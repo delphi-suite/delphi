c7be073a4b7bd1ab3fef0e67fff225ac45ca4d44
Joshua Wendland <80349780+joshuawe@users.noreply.github.com>
2024-02-23 13:29:49 +0100

    add test_label_tokens_from_tokenizer

--- tests/eval/test_token_labelling.py
+++ tests/eval/test_token_labelling.py
@@ -1,7 +1,9 @@
+from pathlib import Path
+
 import pytest
-import spacy
 from spacy.language import Language
 from spacy.tokens import Doc
+from transformers import AutoTokenizer
 
 import delphi.eval.token_labelling as tl
 
@@ -112,3 +114,44 @@ def test_label_batch_sentences(dummy_doc):
     # iterate through tokens in doc
     for token, label in zip(doc, labels[0]):
         assert label == tl.label_single_token(token)
+
+
+def is_valid_structure(obj: dict[int, dict[str, bool]]) -> bool:
+    """
+    Checks whether the obj fits the structure of `dict[int, dict[str, bool]]`. Returns True, if it fits, False otherwise.
+    """
+    if not isinstance(obj, dict):
+        return False
+    for key, value in obj.items():
+        if not isinstance(key, int) or not isinstance(value, dict):
+            return False
+        for sub_key, sub_value in value.items():
+            if not isinstance(sub_key, str) or not isinstance(sub_value, bool):
+                return False
+    return True
+
+
+def test_label_tokens_from_tokenizer():
+    """
+    Simple test, checking if download of tokinzer and the labelling of all tokens in its vocabulary works.
+    """
+    # get a tokinzer
+    model_name = "delphi-suite/delphi-llama2-100k"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    vocab_size = tokenizer.vocab_size
+
+    tokens_str, labelled_token_ids_dict = tl.label_tokens_from_tokenizer(tokenizer)
+    # count the number of lines in the token_str
+    assert tokens_str.count("\n") == (
+        vocab_size + 1
+    )  # each token is on one line + 1 token is '\n'
+    assert len(labelled_token_ids_dict.keys()) == vocab_size
+    assert is_valid_structure(labelled_token_ids_dict) == True
+
+
+# @pytest.mark.parametrize("path", [Path("fsdfsdfsf"), "urghurigh"])
+# def test_import_token_labels(path):
+
+#     labelled_token_ids_dict = tl.import_token_labels(path)
+
+#     # assure that the structure is correct
