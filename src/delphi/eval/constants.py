7683ee9f4b8d08e5b60240051e56620490089fba
JaiDhyani <jaiwithani@gmail.com>
2024-02-23 02:44:53 -0800

    Remove unused constants in delphi/eval/constants.py

--- src/delphi/eval/constants.py
+++ src/delphi/eval/constants.py
@@ -12,27 +12,3 @@ LLAMA2_MODELS = [
     "delphi-llama2-12.8m",
     "delphi-llama2-25.6m",
 ]
-
-LLAMA2_LOGPROB_DATASETS = [
-    "transcendingvictor/delphi-llama2-100k-validation-logprobs",
-    "transcendingvictor/delphi-llama2-200k-validation-logprobs",
-    "transcendingvictor/delphi-llama2-400k-validation-logprobs",
-    "transcendingvictor/delphi-llama2-800k-validation-logprobs",
-    "transcendingvictor/delphi-llama2-1.6m-validation-logprobs",
-    "transcendingvictor/delphi-llama2-3.2m-validation-logprobs",
-    "transcendingvictor/delphi-llama2-6.4m-validation-logprobs",
-    "transcendingvictor/delphi-llama2-12.8m-validation-logprobs",
-    "transcendingvictor/delphi-llama2-25.6m-validation-logprobs",
-]
-
-LLAMA2_HF_MODELS = [
-    "delphi-suite/delphi-llama2-100k",
-    "delphi-suite/delphi-llama2-200k",
-    "delphi-suite/delphi-llama2-400k",
-    "delphi-suite/delphi-llama2-800k",
-    "delphi-suite/delphi-llama2-1.6m",
-    "delphi-suite/delphi-llama2-3.2m",
-    "delphi-suite/delphi-llama2-6.4m",
-    "delphi-suite/delphi-llama2-12.8m",
-    "delphi-suite/delphi-llama2-25.6m",
-]
