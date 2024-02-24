547d648b9fdd7b24413fea2120d0f2aed5da13e3
VICTOR ABIA <transcendingvictor@gmail.com>
2024-02-24 14:02:06 +0100

    mapping sorted and uploaded correctly

--- scripts/map_tokens.py
+++ scripts/map_tokens.py
@@ -40,16 +40,11 @@ if __name__ == "__main__":
         dataset
     )  # outputs the dictionary: dict[int, list[tuple[int, int]]]
 
-    df_dataset = pd.DataFrame.from_dict(
-        {"token_id": list(mapping.keys()), "token_positions": list(mapping.values())}
-    )
-    df_dataset.set_index("token_id", inplace=True)  # Set the token_id as the index
+    complete_mapping = {key: mapping.get(key, None) for key in range(4096)}
 
-    hf_dataset = Dataset.from_pandas(df_dataset)
+    hf_dataset = Dataset.from_dict({"prompt_pos_idx": list(complete_mapping.values())})
 
-    dataset_name = args.dataset_name.split("/")[-1]
-
-    repo_id = f"{args.username}/{dataset_name}-token-map"  # location in to hf
+    repo_id = f"{args.username}/v0-token-map"  # location in to hf
 
     hf_dataset.push_to_hub(
         repo_id=repo_id,
