fe834b03e72f588bef0203f045e45bf0a620b451
Jett <jettjaniak@gmail.com>
2024-02-26 11:49:49 -0800

    rename training_old -> training

--- src/delphi/train/training.sh
+++ src/delphi/train/training.sh
@@ -3,4 +3,4 @@ export CUDA_VISIBLE_DEVICES=3
 export TRANSFORMERS_CACHE=/ceph/jbrinkma/cache/transformers
 export HF_DATASETS_CACHE=/ceph/jbrinkma/cache/datasets
 
-python3 training_old.py --vocab_source=custom  --vocab_size=4096 --max_seq_len=512 --dim=48 --n_layers=8 --n_heads=8 --n_kv_heads=4
+python3 training.py --vocab_source=custom  --vocab_size=4096 --max_seq_len=512 --dim=48 --n_layers=8 --n_heads=8 --n_kv_heads=4
