29048fa4cabcd0a313ca45d8feb0c2ff10b490a6
JaiDhyani <jaiwithani@gmail.com>
2024-03-01 14:56:12 -0800

    adding hf datasets to constants

--- src/delphi/constants.py
+++ src/delphi/constants.py
@@ -1,3 +1,6 @@
 from importlib.resources import files
 
 STATIC_ASSETS_DIR = files("delphi.static")
+
+CORPUS_DATASET = "delphi-suite/tinystories-v2-clean"
+TOKENIZED_CORPUS_DATASET = "delphi-suite/v0-tinystories-v2-clean-tokenized"
