fc3f0215f92fd65764ff7a5412cb9f6caf013ead
JaiDhyani <jaiwithani@gmail.com>
2024-03-11 13:13:57 -0700

    static files tweak

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ setup(
     version="0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    package_data={"delphi.static": ["*"]},
+    package_data={
+        "delphi": ["static/**/*"],
+    },
+    include_package_data=True,
 )
