ab19879a7d3fb3ca0ba84ca44e117dc4aa21d403
JaiDhyani <jaiwithani@gmail.com>
2024-03-11 12:44:02 -0700

    more config_utils

--- src/delphi/train/config_utils.py
+++ src/delphi/train/config_utils.py
@@ -3,13 +3,27 @@ import logging
 from pathlib import Path
 
 from beartype.typing import Any, Iterable
+from platformdirs import user_config_dir
 
 from delphi.constants import CONFIG_PRESETS_DIR
 from delphi.train.gigaconfig import GigaConfig
 
 
-def get_presets() -> Iterable[Path]:
+def get_preset_paths() -> Iterable[Path]:
     return Path(CONFIG_PRESETS_DIR).glob("*.json")  # type: ignore
+
+
+def get_user_config_path() -> Path:
+    _user_config_dir = Path(user_config_dir(appname="delphi"))
+    _user_config_dir.mkdir(parents=True, exist_ok=True)
+    user_config_path = _user_config_dir / "config.json"
+    return user_config_path
+
+
+def get_presets_by_name() -> dict[str, GigaConfig]:
+    return {
+        preset.stem: build_config_from_files([preset]) for preset in get_preset_paths()
+    }
 
 
 def get_configs_in_priority_order(config_files: list[Path]) -> list[dict[str, Any]]:
@@ -43,3 +57,11 @@ def update_config(config: GigaConfig, new_vals: dict[str, Any]):
                 break
         if hasattr(cur, keys[0]):
             setattr(cur, keys[0], val)
+
+
+def build_config_from_files(config_files: list[Path]) -> GigaConfig:
+    configs_in_order = get_configs_in_priority_order(config_files)
+    config = GigaConfig()
+    for _config in configs_in_order:
+        update_config(config, _config)
+    return config
