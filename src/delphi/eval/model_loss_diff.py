edd15840adaab2eb8dea3683c92a82249a946286
menamerai <rphananh@gmail.com>
2024-03-11 15:35:13 -0400

    modified logprobs to loss function to use tensor

--- src/delphi/eval/model_loss_diff.py
+++ src/delphi/eval/model_loss_diff.py
@@ -1,40 +1,22 @@
+import torch
+from beartype.typing import Mapping
+from jaxtyping import Float
+
 # TODO: write tests
 
 
-def next_logprobs_to_loss(next_logprobs: list[list[float]]) -> list[list[float]]:
-    """
-    Given a list of log probabilities, calculate the loss for each token.
-
-    args:
-    - next_logprobs: a list of lists of log probabilities, e.g. [[-0.1, -0.2, ...], [-0.3, -0.4, ...], ...]
-
-    returns: a list of lists of losses, e.g. [[0.1, 0.2, ...], [0.3, 0.4, ...], ...]
-    """
-    next_loss = []
-    for logprobs in next_logprobs:
-        loss = []
-        for logprob in logprobs:
-            if logprob is None:
-                loss.append(0.0)
-            else:
-                # loss is the negative log probability
-                loss.append(-logprob)
-        next_loss.append(loss)
-    return next_loss
-
-
 def next_logprobs_dict_to_loss_dict(
-    next_logprobs: dict[str, list[list[float]]]
-) -> dict[str, list[list[float]]]:
+    next_logprobs_dict: Mapping[str, Float[torch.Tensor, "prompt tok"]]
+) -> dict[str, Float[torch.Tensor, "prompt tok"]]:
     """
-    Given a dictionary of log probabilities mapping to models, calculate the loss for each token and create an appropriate dictionary of losses.
+    Given a dictionary of log probabilities, calculate the loss for each token.
 
     args:
-    - next_logprobs: a dictionary of lists of log probabilities, e.g. {"llama2-100k": [[-0.1, -0.2, ...], [-0.3, -0.4, ...], ...], ...}
+    - next_logprobs_dict: a dictionary of log probabilities, e.g. {"model1": [[-0.1, -0.2, ...], [-0.3, -0.4, ...], ...], "model2": [[-0.1, -0.2, ...], [-0.3, -0.4, ...], ...], ...}
 
-    returns: a dictionary of lists of losses, e.g. {"llama2-100k": [[0.1, 0.2, ...], [0.3, 0.4, ...], ...], ...}
+    returns: a dictionary of losses, e.g. {"model1": [[0.1, 0.2, ...], [0.3, 0.4, ...], ...], "model2": [[0.1, 0.2, ...], [0.3, 0.4, ...], ...], ...}
     """
-    next_loss = {}
-    for key, logprobs in next_logprobs.items():
-        next_loss[key] = next_logprobs_to_loss(logprobs)
-    return next_loss
+    return {
+        model: torch.neg(next_logprobs)
+        for model, next_logprobs in next_logprobs_dict.items()
+    }
