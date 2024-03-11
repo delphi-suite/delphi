bc8b43d499f5382adbdb1325ccee21a9d205d120
JaiDhyani <jaiwithani@gmail.com>
2024-03-11 12:44:30 -0700

    cleanup run_training script

--- scripts/run_training.py
+++ scripts/run_training.py
@@ -1,29 +1,23 @@
 import argparse
-import json
 import logging
 from dataclasses import fields
 from itertools import chain
 from pathlib import Path
-from typing import Any
-
-from platformdirs import user_config_dir
 
 from delphi.constants import CONFIG_PRESETS_DIR
-from delphi.train.config_utils import get_presets, update_config
+from delphi.train.config_utils import (
+    build_config_from_files,
+    get_preset_paths,
+    get_user_config_path,
+    update_config,
+)
 from delphi.train.gigaconfig import GigaConfig
 from delphi.train.training import run_training
 
 
-def get_user_config_path() -> Path:
-    _user_config_dir = Path(user_config_dir(appname="delphi"))
-    _user_config_dir.mkdir(parents=True, exist_ok=True)
-    user_config_path = _user_config_dir / "config.json"
-    return user_config_path
-
-
 def get_preset_args(args: argparse.Namespace) -> list[Path]:
     cands = []
-    for preset in get_presets():
+    for preset in get_preset_paths():
         if hasattr(args, preset.stem) and getattr(args, preset.stem):
             cands.append(preset)
     return cands
@@ -63,7 +57,7 @@ def setup_parser() -> argparse.ArgumentParser:
     )
     config_arg_group = parser.add_argument_group("Config arguments")
     for field in fields(GigaConfig):
-        # test if field is a dataclass
+        # support for nested attributes
         if hasattr(field.type, "__dataclass_fields__"):
             sub_arg_group = parser.add_argument_group(f"Config {field.name} arguments")
             for subfield in fields(field.type):
@@ -81,7 +75,7 @@ def setup_parser() -> argparse.ArgumentParser:
                 help=f"Default: {field.default}",
             )
     preset_arg_group = parser.add_argument_group("Preset configs")
-    for preset in sorted(get_presets()):
+    for preset in sorted(get_preset_paths()):
         preset_arg_group.add_argument(
             f"--{preset.stem}",
             help=f"Use {preset.stem} preset config",
@@ -94,19 +88,8 @@ def main():
     parser = setup_parser()
     args = parser.parse_args()
 
-    # base config
-    config = GigaConfig()
-
-    # config files override default values
     config_files = get_config_files(args)
-    config_dicts = []
-    for config_file in config_files:
-        logging.info(f"Loading {config_file}")
-        with open(config_file, "r") as f:
-            config_dicts.append(json.load(f))
-    config_dicts.sort(key=lambda cd: cd.get("priority", 0))
-    for config_dict in config_dicts:
-        update_config(config, config_dict)
+    config = build_config_from_files(config_files)
     # specific arguments override everything else
     update_config(config, vars(args))
 
