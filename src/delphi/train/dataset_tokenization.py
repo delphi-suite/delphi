83a28dd48b4799a544ef7d6e1445495d5c798d55
Siwei Li <siwli@ea.com>
2024-03-12 19:39:07 -0700

    Split the tokenization function into two parts, fixing the while-loop issues

--- src/delphi/train/dataset_tokenization.py
+++ src/delphi/train/dataset_tokenization.py
@@ -1,39 +1,50 @@
 from collections import deque
-from typing import Union
 
 from transformers import PreTrainedTokenizerBase
 
 
-def get_tokenized_batches(
-    text_stories: Union[list[str], list[list[int]]],
-    tokenizer: PreTrainedTokenizerBase,
+def extend_deque(
+    dq: deque[int],
     context_size: int,
-    input_tokenized=False,
+    text_stories: list[str],
+    prompt_idx: int,
+    tokenizer: PreTrainedTokenizerBase,
+) -> int:
+    while len(dq) < context_size and prompt_idx < len(text_stories):
+        text_story = text_stories[prompt_idx]
+        dq.extend(
+            tokenizer.encode(text_story, add_special_tokens=False)
+            + [tokenizer.eos_token_id]
+        )
+        prompt_idx += 1
+    return prompt_idx
+
+
+def make_new_samples(
+    dq: deque[int], context_size: int, tokenizer: PreTrainedTokenizerBase
 ) -> list[list[int]]:
-    dq = deque()
     samples = []
-
-    prompt_idx = 0
-    while prompt_idx < len(text_stories):
-        while len(dq) < context_size:
-            text_story = text_stories[prompt_idx]
-            if not input_tokenized:
-                dq.extend(
-                    tokenizer.encode(text_story, add_special_tokens=False)
-                    + [tokenizer.eos_token_id]
-                )
-            else:
-                dq.extend(text_story)
-                dq.append(tokenizer.eos_token_id)
-            prompt_idx += 1
-
+    while len(dq) >= context_size:
         sample = [tokenizer.bos_token_id]
-        for i in range(context_size - 1):  # peek at and not pop the last element
+        for _ in range(context_size - 1):  # peek at and not pop the last element
             sample.append(dq.popleft())
         sample.append(dq[0])
-
         samples.append(sample)
+    return samples
 
-    if dq:
-        samples.append([tokenizer.bos_token_id] + list(dq))
+
+def get_tokenized_batches(
+    text_stories: list[str],
+    tokenizer: PreTrainedTokenizerBase,
+    context_size: int,
+) -> list[list[int]]:
+    dq = deque()
+    prompt_idx = 0
+    samples = []
+
+    while prompt_idx < len(text_stories):
+        prompt_idx = extend_deque(dq, context_size, text_stories, prompt_idx, tokenizer)
+        samples.extend(make_new_samples(dq, context_size, tokenizer))
+
+    # We discard the last chunk, so no processing on the remainder of the deque here
     return samples
