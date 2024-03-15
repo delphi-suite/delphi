d5dd7db8141c133daf79b187085845fa86118421
menamerai <rphananh@gmail.com>
2024-03-15 11:32:41 -0400

    added token position tests

--- tests/eval/test_token_position.py
+++ tests/eval/test_token_position.py
@@ -1,16 +1,17 @@
+import math
+
 import pytest
 import torch
 
 from delphi.eval.token_position import (
-    get_all_metrics_from_token_positions,
-    get_all_token_positions_in_category,
-    get_all_tokens_in_category,
+    all_tok_pos_to_metrics_map,
+    get_all_tok_pos_in_category,
 )
 
 
 @pytest.fixture
 def token_mappings():
-    token_labels = {
+    labelled_tokens = {
         0: {
             "Is Noun": True,
             "Is Verb": False,
@@ -29,68 +30,98 @@ def token_mappings():
         3: {
             "Is Noun": True,
             "Is Verb": False,
+            "Is Adjective": False,
+        },
+        4: {
+            "Is Noun": False,
+            "Is Verb": True,
+            "Is Adjective": False,
+        },
+        5: {
+            "Is Noun": False,
+            "Is Verb": False,
+            "Is Adjective": True,
+        },
+        6: {
+            "Is Noun": True,
+            "Is Verb": False,
+            "Is Adjective": False,
+        },
+        7: {
+            "Is Noun": False,
+            "Is Verb": True,
+            "Is Adjective": False,
+        },
+        8: {
+            "Is Noun": False,
+            "Is Verb": False,
             "Is Adjective": True,
         },
     }
 
-    token_positions = {
-        0: [(0, 0), (0, 1)],
-        1: [(0, 2), (0, 3)],
-        2: [(0, 4), (0, 5)],
-        3: [(0, 6), (0, 7)],
+    tok_pos = {
+        0: [],
+        1: [(0, 0), (1, 0)],
+        2: [(0, 1), (1, 1)],
+        3: [(0, 2), (1, 2)],
+        4: [(0, 3), (1, 3)],
+        5: [(0, 4), (1, 4)],
+        6: [(0, 5), (1, 5)],
+        7: [(0, 6), (1, 6)],
+        8: [(0, 7), (1, 7), (0, 8), (0, 9), (1, 7), (1, 8), (1, 9), (1, 10)],
     }
 
-    metrics = torch.tensor(
-        [
-            [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7],
-        ]
+    raw_tok_ids = [
+        torch.Tensor([1, 2, 3, 4, 5, 6, 7, 8, 8, 8]).int(),
+        torch.Tensor([1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8]).int(),
+    ]
+
+    raw_diff_probs = [
+        torch.Tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.8, 0.8]),
+        torch.Tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.8, 0.8, 0.8]),
+    ]
+
+    return labelled_tokens, tok_pos, raw_tok_ids, raw_diff_probs
+
+
+def test_get_all_tok_pos_in_category(token_mappings):
+    labelled_tokens, tok_pos, _, _ = token_mappings
+    noun_positions = get_all_tok_pos_in_category(labelled_tokens, "Is Noun", tok_pos)
+    assert noun_positions == [(0, 2), (1, 2), (0, 5), (1, 5)]
+
+    verb_positions = get_all_tok_pos_in_category(labelled_tokens, "Is Verb", tok_pos)
+    assert verb_positions == [(0, 0), (1, 0), (0, 3), (1, 3), (0, 6), (1, 6)]
+
+    adj_positions = get_all_tok_pos_in_category(
+        labelled_tokens, "Is Adjective", tok_pos
     )
+    # fmt: off
+    assert adj_positions == [(0, 1), (1, 1), (0, 4), (1, 4), (0, 7), (1, 7), (0, 8), (0, 9), (1, 7), (1, 8), (1, 9), (1, 10)]
+    # fmt: on
 
-    return token_labels, token_positions, metrics
-
-
-def test_get_all_tokens_in_category(token_mappings):
-    token_labels, _, _ = token_mappings
-
-    result = get_all_tokens_in_category(token_labels, "nouns")
-    assert result == [0, 3]
-    result = get_all_tokens_in_category(token_labels, "verbs")
-    assert result == [1]
-    result = get_all_tokens_in_category(token_labels, "adjectives")
-    assert result == [2, 3]
+    with pytest.raises(KeyError):
+        # for now this raises KeyError, might want to try and catch this
+        get_all_tok_pos_in_category(labelled_tokens, "Is Adverb", tok_pos)
 
 
-def test_get_all_token_positions_in_category(token_mappings):
-    token_labels, token_positions, _ = token_mappings
+def test_all_tok_pos_to_metrics_map(token_mappings):
+    labelled_tokens, tok_pos, raw_tok_ids, raw_diff_probs = token_mappings
+    noun_positions = get_all_tok_pos_in_category(labelled_tokens, "Is Noun", tok_pos)
+    noun_metrics = all_tok_pos_to_metrics_map(noun_positions, raw_diff_probs)
+    # use math.isclose to compare floats
+    for k, v in noun_metrics.items():
+        assert math.isclose(v, raw_diff_probs[k[0]][k[1]].item())
 
-    result = get_all_token_positions_in_category(token_labels, token_positions, "nouns")
-    assert result == {
-        0: [(0, 0), (0, 1)],
-        3: [(0, 6), (0, 7)],
-    }
-    result = get_all_token_positions_in_category(token_labels, token_positions, "verbs")
-    assert result == {
-        1: [(0, 2), (0, 3)],
-    }
-    result = get_all_token_positions_in_category(
-        token_labels, token_positions, "adjectives"
+    verb_positions = get_all_tok_pos_in_category(labelled_tokens, "Is Verb", tok_pos)
+    verb_metrics = all_tok_pos_to_metrics_map(verb_positions, raw_diff_probs)
+    for k, v in verb_metrics.items():
+        assert math.isclose(v, raw_diff_probs[k[0]][k[1]].item())
+
+    adj_positions = get_all_tok_pos_in_category(
+        labelled_tokens, "Is Adjective", tok_pos
     )
-    assert result == {
-        2: [(0, 4), (0, 5)],
-        3: [(0, 6), (0, 7)],
-    }
-
-
-def test_get_all_metrics_from_token_positions(token_mappings):
-    _, token_positions, metrics = token_mappings
-
-    result = get_all_metrics_from_token_positions(token_positions, metrics)
-    expected = {
-        0: torch.tensor([0.0, 0.1]),
-        1: torch.tensor([0.2, 0.3]),
-        2: torch.tensor([0.4, 0.5]),
-        3: torch.tensor([0.6, 0.7]),
-    }
-
-    for k, v in result.items():
-        assert torch.allclose(v, expected[k])
+    adj_metrics = all_tok_pos_to_metrics_map(
+        adj_positions, raw_diff_probs, quantile_start=0.5, quantile_end=0.8
+    )
+    for k, v in adj_metrics.items():
+        assert math.isclose(v, raw_diff_probs[k[0]][k[1]].item())
