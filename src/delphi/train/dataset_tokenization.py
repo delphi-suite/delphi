0755f80d9b49e96f5d17136717d14f28e4d7d5f1
Siwei Li <siwli@ea.com>
2024-03-17 18:35:53 -0700

    Add docstrings to the functions

--- src/delphi/train/dataset_tokenization.py
+++ src/delphi/train/dataset_tokenization.py
@@ -10,6 +10,23 @@ def extend_deque(
     prompt_idx: int,
     tokenizer: PreTrainedTokenizerBase,
 ) -> int:
+    """
+    Extends the deque with tokenized text stories until the deque grows large
+    enough to reach the context size, or until all text stories are processed.
+
+    The usage of a deque here aims to save the memory as opposed to
+    load all the stories and tokenize them at once.
+
+    Args:
+        dq (deque[int]): Deque to extend with tokenized tokens.
+        context_size (int): Size of the context(input sequences).
+        text_stories (list[str]): List of (untokenized) text stories to be tokenized.
+        prompt_idx (int): Index of the current text story.
+        tokenizer (PreTrainedTokenizerBase): Tokenizer to encode the text strings.
+
+    Returns:
+        int: Updated index in the text stories dataset.
+    """
     while len(dq) < context_size and prompt_idx < len(text_stories):
         text_story = text_stories[prompt_idx]
         dq.extend(
@@ -23,6 +40,22 @@ def extend_deque(
 def make_new_samples(
     dq: deque[int], context_size: int, tokenizer: PreTrainedTokenizerBase
 ) -> list[list[int]]:
+    """
+    Generates new samples for training by creating sequences of tokens
+    from the deque until the deque is empty.
+
+    Note: the model is unable to use the last token in an input sequence,
+    so we repeat this token in the next input sequence.
+
+    Args:
+        dq (deque[int]): Deque containing tokenized tokens.
+        context_size (int): Size of the context (input sequences).
+        tokenizer (PreTrainedTokenizerBase): Tokenizer to encode the text strings.
+
+    Returns:
+        list[list[int]]: List of token sequences of the same length(context_size).
+    """
+
     samples = []
     while len(dq) >= context_size:
         sample = [tokenizer.bos_token_id]
@@ -38,6 +71,19 @@ def get_tokenized_batches(
     tokenizer: PreTrainedTokenizerBase,
     context_size: int,
 ) -> list[list[int]]:
+    """
+    Tokenizes the input text stories using the provided tokenizer and
+    generates token sequences of the specified length.
+
+    Args:
+        text_stories (list[str]): List of text stories to be tokenized.
+        tokenizer (PreTrainedTokenizerBase): Tokenizer to encode the text strings.
+        context_size (int): Size of the context (input sequences).
+
+    Returns:
+        list[list[int]]: List of token sequences of length equal to context_size.
+    """
+
     dq = deque()
     prompt_idx = 0
     samples = []
